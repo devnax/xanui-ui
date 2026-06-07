5aa4483d6d210c0357f3bc7fb63158e4ed99edf1
Naxrul Ahmed <devnaxrul@gmail.com>
2026-06-07 07:25:21 +0600

    fix divider base class

--- src/Divider/index.tsx
+++ src/Divider/index.tsx
@@ -1,39 +1,54 @@
-"use client"
-import React from 'react';
-import { Tag, TagProps, TagComponentType, UseColorTemplateColor, useBreakpointProps, useBreakpointPropsType } from '@xanui/core';
+"use client";
+import React from "react";
+import {
+  Tag,
+  TagProps,
+  TagComponentType,
+  UseColorTemplateColor,
+  useBreakpointProps,
+  useBreakpointPropsType,
+} from "@xanui/core";
 
+export type DividerProps<T extends TagComponentType = "div"> = Omit<
+  TagProps<T>,
+  "color"
+> & {
+  direction?: useBreakpointPropsType<"verticle" | "horizental">;
+  color?: useBreakpointPropsType<UseColorTemplateColor>;
+  size?: useBreakpointPropsType<number>;
+};
 
-export type DividerProps<T extends TagComponentType = "div"> = Omit<TagProps<T>, "color"> & {
-    direction?: useBreakpointPropsType<"verticle" | "horizental">;
-    color?: useBreakpointPropsType<UseColorTemplateColor>;
-    size?: useBreakpointPropsType<number>;
-}
+const Divider = React.forwardRef(
+  <T extends TagComponentType = "div">(
+    { children, direction, color, size, ...rest }: DividerProps<T>,
+    ref: React.Ref<any>,
+  ) => {
+    const _p: any = {};
+    if (direction) _p.direction = direction;
+    if (color) _p.color = color;
+    if (size) _p.size = size;
+    const p: any = useBreakpointProps(_p);
+    direction = p.direction ?? "horizental";
+    color = p.color ?? "default";
+    size = p.size ?? 1;
 
-const Divider = React.forwardRef(<T extends TagComponentType = "div">({ children, direction, color, size, ...rest }: DividerProps<T>, ref: React.Ref<any>) => {
-    const _p: any = {}
-    if (direction) _p.direction = direction
-    if (color) _p.color = color
-    if (size) _p.size = size
-    const p: any = useBreakpointProps(_p)
-    direction = p.direction ?? "horizental"
-    color = p.color ?? 'default'
-    size = p.size ?? 1
-
-    let isHori = direction === 'horizental'
+    let isHori = direction === "horizental";
 
     return (
-        <Tag
-            {...rest}
-            baseClass='default.divider'
-            sxr={{
-                width: isHori ? "100%" : size,
-                height: isHori ? size : "100%",
-                bgcolor: color === 'default' ? `default.divider` : `${color}.divider`,
-            }}
-            ref={ref}
-        >{children}</Tag>
-    )
-})
+      <Tag
+        {...rest}
+        baseClass="divider"
+        sxr={{
+          width: isHori ? "100%" : size,
+          height: isHori ? size : "100%",
+          bgcolor: color === "default" ? `default.divider` : `${color}.divider`,
+        }}
+        ref={ref}
+      >
+        {children}
+      </Tag>
+    );
+  },
+);
 
-export default Divider
-
+export default Divider;
