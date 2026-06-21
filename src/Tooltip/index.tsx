931566ce28eef777dba3db4d408edf8ccc1aaf7e
Naxrul Ahmed <devnaxrul@gmail.com>
2026-06-21 07:48:10 +0600

    updated Tooltip

--- src/Tooltip/index.tsx
+++ src/Tooltip/index.tsx
@@ -1,59 +1,90 @@
 "use client";
-import { Children, cloneElement, ReactElement, useState } from 'react'
-import Menu, { MenuProps } from '../Menu'
-import { useBreakpointProps, useColorTemplate, UseColorTemplateColor, UseColorTemplateType, useBreakpointPropsType } from '@xanui/core'
-
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  ReactElement,
+  useState,
+} from "react";
+import Menu, { MenuProps } from "../Menu";
+import {
+  useBreakpointProps,
+  useColorTemplate,
+  UseColorTemplateColor,
+  UseColorTemplateType,
+  useBreakpointPropsType,
+} from "@xanui/core";
+import Text, { TextProps } from "../Text";
 
 export type TooltipProps = {
-    children: ReactElement;
-    title: useBreakpointPropsType<string>;
-    color?: useBreakpointPropsType<UseColorTemplateColor>;
-    variant?: useBreakpointPropsType<UseColorTemplateType>;
-    placement?: MenuProps['placement']
-}
+  children: ReactElement;
+  title: useBreakpointPropsType<string | ReactElement>;
+  color?: useBreakpointPropsType<UseColorTemplateColor>;
+  variant?: useBreakpointPropsType<UseColorTemplateType>;
+  placement?: MenuProps["placement"];
+  slotProps?: {
+    title?: Omit<TextProps, "children">;
+    menu: Omit<MenuProps, "target" | "chilcren" | "placement">;
+  };
+};
 
-const Tooltip = ({ children, title, variant, color, placement }: TooltipProps) => {
-    const [target, setTarget] = useState<any>()
-    const _p: any = {}
-    if (title) _p.title = title
-    if (color) _p.color = color
-    if (variant) _p.variant = variant
-    const p: any = useBreakpointProps(_p)
-    title = p.title
-    color = p.color ?? 'default'
-    variant = p.variant ?? "fill"
-    placement ??= "bottom"
+const Tooltip = ({
+  children,
+  title,
+  variant,
+  color,
+  placement,
+  slotProps,
+}: TooltipProps) => {
+  const [target, setTarget] = useState<any>();
+  const _p: any = {};
+  if (title) _p.title = title;
+  if (color) _p.color = color;
+  if (variant) _p.variant = variant;
+  const p: any = useBreakpointProps(_p);
+  title = p.title;
+  color = p.color ?? "default";
+  variant = p.variant ?? "fill";
+  placement ??= "bottom";
 
-    const template = useColorTemplate(color as any, variant as any)
-    if (!children || Array.isArray(children)) throw new Error("Invalid children in Toast")
-    const first: any = Children.toArray(children).shift();
-    const child = cloneElement(first, {
-        onMouseEnter: (e) => {
-            setTarget(e.target)
-        },
-        onMouseLeave: () => setTarget(null)
-    })
+  const template = useColorTemplate(color as any, variant as any);
+  if (!children || Array.isArray(children))
+    throw new Error("Invalid children in Toast");
+  const first: any = Children.toArray(children).shift();
+  const child = cloneElement(first, {
+    onMouseEnter: (e) => {
+      setTarget(e.target);
+    },
+    onMouseLeave: () => setTarget(null),
+  });
 
+  return (
+    <>
+      {child}
+      <Menu
+        target={target}
+        placement={placement}
+        {...slotProps?.menu}
+        slotProps={{
+          ...slotProps?.menu?.slotProps,
+          content: {
+            p: 0.5,
+            shadow: 1,
+            ...template.main,
+            bgcolor:
+              color == "default" ? "default.base" : template.main.bgcolor,
+            ...slotProps?.menu.slotProps?.content,
+          },
+        }}
+      >
+        {isValidElement(title) ? (
+          title
+        ) : (
+          <Text {...slotProps?.title}>{title as any}</Text>
+        )}
+      </Menu>
+    </>
+  );
+};
 
-    return (
-        <>
-            {child}
-            <Menu
-                target={target}
-                placement={placement}
-                slotProps={{
-                    content: {
-                        p: .5,
-                        shadow: 1,
-                        ...template.main,
-                        bgcolor: color == 'default' ? "default.base" : template.main.bgcolor,
-                    }
-                }}
-            >
-                {title as any}
-            </Menu>
-        </>
-    )
-}
-
-export default Tooltip
+export default Tooltip;
